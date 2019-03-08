d8913500a1fb23ac9be324c33c5f1259f5287f3a
Dudut - Techstars <carlos.eduardo.lage@hotmail.com>
2019-03-07 22:31:10 -0300

    v0.00

---
+++ App/Controllers/login.php
@@ -0,0 +1,16 @@
+<?php
+
+class login
+{
+    public function showPage()
+    {
+        require_once 'App/Views/index.php';
+
+        return 0;
+    }
+
+    public function __construct()
+    {
+        return 0;
+    }
+}
